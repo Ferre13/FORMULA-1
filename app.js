bdada6b2abbdcfcb6f4d8870735343266985e736
Ferre13 <131792152+Ferre13@users.noreply.github.com>
2024-09-26 00:40:30 +0200

    vuelta rapida

--- app.js
+++ app.js
@@ -26,6 +26,39 @@ document.addEventListener('DOMContentLoaded', function () {
         'Alpine F1 Team': 'img/alpine.png',
     };
 
+    let isFastestLapSorted = false; // Estado de orden para "Fastest Lap"
+    let raceData = null; // Almacenar los datos de la carrera
+
+    // Función para alternar el ícono de la flecha en el encabezado "Fastest Lap"
+    function toggleFastestLapIcon() {
+        const fastestLapIcon = document.getElementById('fastest-lap-icon');
+        fastestLapIcon.textContent = isFastestLapSorted ? '▼' : '▲'; // Cambia el icono
+    }
+
+    // Función para ordenar los resultados por "Fastest Lap" (de menor a mayor)
+    function sortResultsByFastestLap(resultsByDriver) {
+        const sortedDrivers = Object.keys(resultsByDriver).sort((a, b) => {
+            const lapTimeA = resultsByDriver[a][0].fastestLapTime === 'N/A' ? Infinity : parseTime(resultsByDriver[a][0].fastestLapTime);
+            const lapTimeB = resultsByDriver[b][0].fastestLapTime === 'N/A' ? Infinity : parseTime(resultsByDriver[b][0].fastestLapTime);
+            return lapTimeA - lapTimeB; // Orden de menor a mayor
+        });
+
+        // Reorganizar los datos según el orden
+        const sortedResults = {};
+        sortedDrivers.forEach(driver => {
+            sortedResults[driver] = resultsByDriver[driver];
+        });
+
+        // Renderizar los datos ordenados
+        renderData({ ...raceData, resultsByDriver: sortedResults });
+    }
+
+    // Función para convertir el tiempo de vuelta en milisegundos
+    function parseTime(timeStr) {
+        const [minutes, seconds] = timeStr.split(':').map(parseFloat);
+        return (minutes * 60 + seconds) * 1000;
+    }
+
     // Función para obtener datos de la API
     function fetchDataFromAPI() {
         console.log('Fetching data from API...');
@@ -48,73 +81,73 @@ document.addEventListener('DOMContentLoaded', function () {
                     localStorage.setItem('raceData', JSON.stringify(data));
                     const endTime = performance.now();
                     console.log(`API Data fetched in ${endTime - startTime} ms`);
-                    return data;
+                    raceData = data; // Almacenar datos de la carrera
+                    renderData(data);
                 } else {
                     throw new Error('No data returned from API');
                 }
             });
     }
 
-// Función para analizar datos XML
-function parseXMLData(xmlDoc) {
-    const raceName = getTextContent(xmlDoc, 'RaceName');
-    const circuitName = getTextContent(xmlDoc, 'CircuitName');
-    const location = `${getTextContent(xmlDoc, 'Locality')}, ${getTextContent(xmlDoc, 'Country')}`;
+    // Función para analizar datos XML
+    function parseXMLData(xmlDoc) {
+        const raceName = getTextContent(xmlDoc, 'RaceName');
+        const circuitName = getTextContent(xmlDoc, 'CircuitName');
+        const location = `${getTextContent(xmlDoc, 'Locality')}, ${getTextContent(xmlDoc, 'Country')}`;
 
-    const resultsByDriver = {};
-    xmlDoc.querySelectorAll('Result').forEach(result => {
-        const givenName = getTextContent(result, 'GivenName');
-        const familyName = getTextContent(result, 'FamilyName');
-        const fullName = `${givenName} ${familyName}`;
+        const resultsByDriver = {};
+        xmlDoc.querySelectorAll('Result').forEach(result => {
+            const givenName = getTextContent(result, 'GivenName');
+            const familyName = getTextContent(result, 'FamilyName');
+            const fullName = `${givenName} ${familyName}`;
 
-        if (!resultsByDriver[fullName]) {
-            resultsByDriver[fullName] = [];
-        }
-
-        const position = result.getAttribute('position');
-        let gridPosition = getTextContent(result, 'Grid');
-        if (gridPosition === '0') {
-            gridPosition = 'PIT';
-        } else {
-            gridPosition = gridPosition || 'N/A';
-        }
-
-        const constructor = getTextContent(result, 'Constructor Name');
-        const status = getTextContent(result, 'Status');
-        let time = getTextContent(result, 'Time');
-        const fastestLapTime = getTextContent(result, 'FastestLap Time') || 'N/A';
-        const fastestLap = result.querySelector('FastestLap');
-        const fastestLapPosition = fastestLap ? fastestLap.getAttribute('rank') : 'N/A';
-
-        if (position === 'D') {
-            time = `${time} (DSQ)`;
-        } else if (status !== 'Finished') {
-            if (status.includes('+')) {
-                time = status;
-            } else {
-                time = `${status} (DNF)`;
+            if (!resultsByDriver[fullName]) {
+                resultsByDriver[fullName] = [];
             }
-        }
-        
 
-        resultsByDriver[fullName].push({
-            position,
-            gridPosition,
-            constructor,
-            time,
-            fastestLapTime,
-            fastestLapPosition,
+            const position = result.getAttribute('position');
+            let gridPosition = getTextContent(result, 'Grid');
+            if (gridPosition === '0') {
+                gridPosition = 'PIT';
+            } else {
+                gridPosition = gridPosition || 'N/A';
+            }
+
+            const constructor = getTextContent(result, 'Constructor Name');
+            const status = getTextContent(result, 'Status');
+            let time = getTextContent(result, 'Time');
+            const fastestLapTime = getTextContent(result, 'FastestLap Time') || 'N/A';
+            const fastestLap = result.querySelector('FastestLap');
+            const fastestLapPosition = fastestLap ? fastestLap.getAttribute('rank') : 'N/A';
+
+            if (position === 'D') {
+                time = `${time} (DSQ)`;
+            } else if (status !== 'Finished') {
+                if (status.includes('+')) {
+                    time = status;
+                } else {
+                    time = `${status} (DNF)`;
+                }
+            }
+            
+            resultsByDriver[fullName].push({
+                position,
+                gridPosition,
+                constructor,
+                time,
+                fastestLapTime,
+                fastestLapPosition,
+            });
         });
-    });
 
-    return { raceName, circuitName, location, resultsByDriver };
-}
+        return { raceName, circuitName, location, resultsByDriver };
+    }
 
-// Función para obtener el contenido de texto de un elemento
-function getTextContent(element, tagName) {
-    const targetElement = element.querySelector(tagName);
-    return targetElement ? targetElement.textContent.trim() : 'N/A';
-}
+    // Función para obtener el contenido de texto de un elemento
+    function getTextContent(element, tagName) {
+        const targetElement = element.querySelector(tagName);
+        return targetElement ? targetElement.textContent.trim() : 'N/A';
+    }
 
     // Función para renderizar los datos
     function renderData(data) {
@@ -161,7 +194,7 @@ function getTextContent(element, tagName) {
                                     <th style="text-align: left; text-transform: uppercase;">Position/Driver</th>
                                     <th style="text-transform: uppercase;">Constructor</th>
                                     <th style="text-transform: uppercase;">Time</th>
-                                    <th style="text-transform: uppercase;">Fastest Lap</th>
+                                    <th id="fastest-lap-header" style="cursor: pointer; text-transform: uppercase;">Fastest Lap <span id="fastest-lap-icon">▼</span></th>
                                     <th style="text-transform: uppercase;">Grid</th>
                                 </tr>
                             </thead>
@@ -211,6 +244,18 @@ function getTextContent(element, tagName) {
                 constructorCell.style.color = teamColors[team];
             }
         });
+
+        // Agregar evento de clic para ordenar por "Fastest Lap"
+        document.getElementById('fastest-lap-header').addEventListener('click', () => {
+            if (isFastestLapSorted) {
+                // Volver al orden original de la carrera
+                renderData(raceData);
+                isFastestLapSorted = false;
+            } else {
+                isFastestLapSorted = true; // Activar el estado de orden
+                sortResultsByFastestLap(resultsByDriver); // Ordenar y renderizar
+            }
+        });
     }
 
     // Función para cargar y renderizar los datos desde localStorage
@@ -243,9 +288,7 @@ function getTextContent(element, tagName) {
     loadFromLocalStorage();
 
     // Luego, intentar obtener los datos de la API
-    fetchDataFromAPI().then(data => {
-        renderData(data);
-    }).catch(error => {
+    fetchDataFromAPI().catch(error => {
         console.error('Error fetching data from API:', error);
     });
 });
